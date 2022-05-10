c0c6a03b2df882b8651d065c0ce7c9a1fee8e690
aquashdw <aquashdw@gmail.com>
2022-05-10 14:49:07 +0900

    finish demo

--- delivery/models.py
+++ delivery/models.py
@@ -1,8 +1,24 @@
+from unicodedata import name
 from django.db import models
 
 
 class Diner(models.Model):
     name = models.CharField(max_length=32)
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=32)
+    diners = models.ManyToManyField(
+        Diner, 
+        through="DeliveryArea",
+        related_name="deliver_area"
+    )
+
+
+class DeliveryArea(models.Model):
+    diner = models.ForeignKey(Diner, on_delete=models.CASCADE, related_name="delivery_area")
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, related_name="area_fee")
+    fee = models.IntegerField(default=1000)
 
 
 class Menu(models.Model):
@@ -12,4 +28,25 @@ class Menu(models.Model):
         Diner, 
         on_delete=models.CASCADE, 
         related_name="diner_menus"
+    )
+
+
+class MenuOptionGroup(models.Model):
+    name = models.CharField(max_length=32)
+    mandatory = models.BooleanField(default=False)
+    select_min = models.IntegerField(default=0)
+    select_max = models.IntegerField(default=1)
+    for_menu = models.ForeignKey(
+        Menu, 
+        on_delete=models.CASCADE, 
+        related_name="menu_option_group"
+    )
+
+
+
+class FoodCategory(models.Model):
+    name = models.CharField(max_length=32)
+    category_diners = models.ManyToManyField(
+        Diner,
+        related_name="diner_categories"
     )
--- delivery/views.py
+++ delivery/views.py
@@ -1,26 +1,58 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404, get_list_or_404
 from django.http import JsonResponse
 
 from delivery.models import *
 
 
-def create_diner_view(request):
-    if request.method == "POST":
-        diner_name = request.POST.get("name")
-        new_diner = Diner()
-        new_diner.name
-        new_diner.save()
-        return redirect("/")
-    return render(request, "temp.html")
-
-
 def test(request):
-    diner_info = create_diner("test_diner")
+    category_han = create_category("hansik")
+    category_bun = create_category("bunsik")
+    category_pun = create_category("pyunsik")
+
+    area_sun = create_area("선릉")
+    area_sam = create_area("삼성")
+    area_seo = create_area("서초")
+
+    diner_info = create_diner("test_diner_1")
     menu_info = create_menu("test_menu_1", 500, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
     menu_info = create_menu("test_menu_2", 1000, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
     menu_info = create_menu("test_menu_3", 1500, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
+    set_diner_category(diner_info.get("id"), category_han.get("id"))
+    set_diner_category(diner_info.get("id"), category_bun.get("id"))
+    set_diner_delivery_area_1(diner_info.get("id"), area_sun.get("id"), fee=5000)
+    set_diner_delivery_area_2(diner_info.get("id"), area_sam.get("id"), fee=6000)
+
     get_menu_diner(menu_info.get("id"))
     get_diner_menus(diner_info.get("id"))
+    get_diner_categories(diner_info.get("id"))
+    get_diner_delivery_fees_per_area(diner_info.get("id"))
+
+    diner_info = create_diner("test_diner_2")
+    menu_info = create_menu("test_menu_4", 500, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
+    menu_info = create_menu("test_menu_5", 1000, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
+    menu_info = create_menu("test_menu_6", 1500, diner_info.get("id"))
+    create_menu_option_group("size", True, menu_info.get("id"))
+    create_menu_option_group("sari", False, menu_info.get("id"), select_max=10)
+    set_diner_category(diner_info.get("id"), category_han.get("id"))
+    set_diner_category(diner_info.get("id"), category_pun.get("id"))
+    set_diner_delivery_area_1(diner_info.get("id"), area_sun.get("id"), fee=5000)
+    set_diner_delivery_area_2(diner_info.get("id"), area_seo.get("id"), fee=10000)
+    
+    get_menu_diner(menu_info.get("id"))
+    get_diner_menus(diner_info.get("id"))
+    get_diner_categories(diner_info.get("id"))
+    get_diner_delivery_fees_per_area(diner_info.get("id"))
+
     return JsonResponse({
         "status": "success"
     })
@@ -50,6 +82,30 @@ def create_menu(name, price, diner_id):
     }
 
 
+def create_menu_option_group(name, mandatory, menu_id, select_min=0, select_max=1):
+    new_menu_option_group = MenuOptionGroup()
+    new_menu_option_group.name = name
+    new_menu_option_group.mandatory = mandatory
+    new_menu_option_group.for_menu = get_object_or_404(Menu, pk=menu_id)
+    new_menu_option_group.select_min = select_min
+    new_menu_option_group.select_max = select_max
+    new_menu_option_group.save()
+
+
+def create_category(name):
+    new_category = FoodCategory()
+    new_category.name = name
+    new_category.save()
+    return { "id": new_category.id }
+
+
+def create_area(name):
+    new_area = Area()
+    new_area.name = name
+    new_area.save()
+    return { "id": new_area.id }
+
+
 def get_menu_diner(menu_id):
     menu = get_object_or_404(Menu, id=menu_id)
     diner = menu.diner
@@ -61,3 +117,64 @@ def get_diner_menus(diner_id):
     menu_list = diner.diner_menus.all()
     for menu in menu_list:
         print(f"this diner has menu: {menu.name}")
+
+
+def get_diner_categories(diner_id):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    diner_categories = target_diner.diner_categories.all()
+    for category in diner_categories:
+        print(f"the category of this diner: {category.name}")
+
+
+def get_category_diners(category_id):
+    target_category = get_object_or_404(FoodCategory, id=category_id)
+    category_diners = target_category.category_diners.all()
+    for diner in category_diners:
+        print(f"diners under this category: {diner.name}")
+
+
+def get_diner_delivery_area(diner_id):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    diner_delivery_area = target_diner.deliver_area.all()
+    for area in diner_delivery_area:
+        print(f"this diner delivers to: {area.name}")
+
+
+def get_area_delivering_diners(area_id):
+    target_area = get_object_or_404(Area, id=area_id)
+    diners_in_area = target_area.diners.all()
+    for diner in diners_in_area:
+        print(f"this area has diner: {diner.name}")
+
+
+def get_diner_delivery_fees_per_area(diner_id):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    diner_delivery_area = get_list_or_404(DeliveryArea, diner=target_diner)
+    for delivery_area in diner_delivery_area:
+        print(f"to deliver to: {(delivery_area.area).name}, costs: {delivery_area.fee}")
+
+
+def set_diner_category(diner_id, category_id):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    target_category = get_object_or_404(FoodCategory, id=category_id)
+    target_category.category_diners.add(target_diner)
+    target_category.save()
+
+
+def set_diner_delivery_area_1(diner_id, area_id, fee=1000):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    target_area = get_object_or_404(Area, id=area_id)
+    new_delivery_area = DeliveryArea()
+    new_delivery_area.diner = target_diner
+    new_delivery_area.area = target_area
+    new_delivery_area.fee = fee
+    new_delivery_area.save()
+
+
+def set_diner_delivery_area_2(diner_id, area_id, fee=1000):
+    target_diner = get_object_or_404(Diner, id=diner_id)
+    target_area = get_object_or_404(Area, id=area_id)
+    target_diner.deliver_area.add(target_area, through_defaults={
+        "fee": fee,
+    })
+    target_diner.save()
