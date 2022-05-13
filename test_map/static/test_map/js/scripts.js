0dabebc00599d6299e55cb7ee6bed5743982d62f
aquashdw <aquashdw@gmail.com>
2022-05-13 10:07:45 +0900

    minor update (find cookie)

--- test_map/static/test_map/js/scripts.js
+++ test_map/static/test_map/js/scripts.js
@@ -3,8 +3,12 @@ function drawCenterMarker(map){
     currentCenterMarker = drawNewMarker(map, map.getCenter().x, map.getCenter().y);
 }
 
-function getCsrfToken(){
+function findInCookie(cookieName){
     return document.cookie.split(";")
-        .find((item) => item.includes("csrftoken"))
+        .find((item) => item.includes(cookieName))
         .split("=")[1];
+}
+
+function getCsrfToken(){
+    return findInCookie("csrftoken")
 }
